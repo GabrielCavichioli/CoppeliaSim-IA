7d04874c1462b8759c5d6a875f0156ddbf772889
Cavichioli <gabrielcavichioli29@gmail.com>
2024-05-15 09:32:36 -0300

    Realizando uma API com o Coppelia

--- API.py
+++ API.py
@@ -1,1 +1,14 @@
 # Autor: Gabriel Antonio Cavichioli | Data: 15/05/24.
+
+from coppeliasim_zmqremoteapi_client import RemoteAPIClient
+
+client = RemoteAPIClient()
+sim = client.require('sim')
+
+sim.setStepping(True)
+
+sim.startSimulation()
+while (t := sim.getSimulationTime()) < 50:
+    print(f'Simulation time: {t:.2f} [s]')
+    sim.step()
+sim.stopSimulation()
